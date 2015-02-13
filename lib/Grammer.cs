e2fb7129e39d1fe8605d5a8597bb412adb5cd628
Chukwudi Derek Uche <cuche@purdue.edu>
2015-02-12 22:48:57 -0500

    Changed some stuff in grammer.cs

--- lib/Grammer.cs
+++ lib/Grammer.cs
@@ -2,33 +2,35 @@
 using System.Speech.SrgsGrammar;
 using System.Speeech.Recognition;
 
-public class ButlerGrammar
-{
-    private Grammar wolfram = new Grammar(new GrammarBuilder("wolfram"));
+namespace Butler {
+    public class ButlerGrammar
+    {
+        private Grammar wolfram = new Grammar(new GrammarBuilder("wolfram"));
 
-    private Grammar open = new Grammar(new GrammarBuilder("open"));
-    //GrammarBuilder _run = new GrammarBuilder("run");
+        private Grammar open = new Grammar(new GrammarBuilder("open"));
+        //GrammarBuilder _run = new GrammarBuilder("run");
     
-    private Grammar google = new Grammar(new GrammarBuilder("google"));
-    //GrammarBuilder _search = new GrammarBuilder("search");
+        private Grammar google = new Grammar(new GrammarBuilder("google"));
+        //GrammarBuilder _search = new GrammarBuilder("search");
 
-    private Grammar delete = new Grammar(new GrammarBuilder("delete"));
-    //GrammarBuilder _remove = new GrammarBuilder("remove");
-    //GrammarBuilder _erase = new GrammarBuilder("erase");
+        private Grammar delete = new Grammar(new GrammarBuilder("delete"));
+        //GrammarBuilder _remove = new GrammarBuilder("remove");
+        //GrammarBuilder _erase = new GrammarBuilder("erase");
 
-    private Grammar kill = new Grammar(new GrammarBuilder("kill"));
-    //GrammarBuilder _quit = new GrammarBuilder("quit");
+        private Grammar kill = new Grammar(new GrammarBuilder("kill"));
+        //GrammarBuilder _quit = new GrammarBuilder("quit");
 
-    private Grammar wiki = new Grammar(new GrammarBuilder("wiki"));
+        private Grammar wiki = new Grammar(new GrammarBuilder("wiki"));
 
-    private Grammar tweet = new Grammar(new GrammarBuilder("tweet"));
+        private Grammar tweet = new Grammar(new GrammarBuilder("tweet"));
 
-    private Grammar recommend = new Grammar(new GrammarBuilder("recommend"));
-    //GrammarBuilder _movie = new GrammarBuilder("movie");
+        private Grammar recommend = new Grammar(new GrammarBuilder("recommend"));
+        //GrammarBuilder _movie = new GrammarBuilder("movie");
 
-	public Grammar[] getGrammar()
-	{
-        Grammar[] grammar = { wolfram, open, google, delete, kill, wiki, tweet, recommend };
-        return grammar;
-	}
+	    public Grammar[] getGrammar()
+	    {
+            Grammar[] grammar = { wolfram, open, google, delete, kill, wiki, tweet, recommend };
+            return grammar;
+	    }
+    }
 }
