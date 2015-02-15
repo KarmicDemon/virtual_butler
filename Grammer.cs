81362db944740b4860556fe6b45c1c632941399a
Chukwudi Derek Uche <cuche@purdue.edu>
2015-02-14 21:52:44 -0500

    Now to figure out why this will not build

---
+++ Grammer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Speech.SrgsGrammar;
+using System.Speeech.Recognition;
+
+public class ButlerGrammar
+{
+    private Grammar wolfram = new Grammar(new GrammarBuilder("wolfram"));
+
+    private Grammar open = new Grammar(new GrammarBuilder("open"));
+    //GrammarBuilder _run = new GrammarBuilder("run");
+
+    private Grammar google = new Grammar(new GrammarBuilder("google"));
+    //GrammarBuilder _search = new GrammarBuilder("search");
+
+    private Grammar delete = new Grammar(new GrammarBuilder("delete"));
+    //GrammarBuilder _remove = new GrammarBuilder("remove");
+    //GrammarBuilder _erase = new GrammarBuilder("erase");
+
+    private Grammar kill = new Grammar(new GrammarBuilder("kill"));
+    //GrammarBuilder _quit = new GrammarBuilder("quit");
+
+    private Grammar wiki = new Grammar(new GrammarBuilder("wiki"));
+
+    private Grammar tweet = new Grammar(new GrammarBuilder("tweet"));
+
+    private Grammar recommend = new Grammar(new GrammarBuilder("recommend"));
+    //GrammarBuilder _movie = new GrammarBuilder("movie");
+
+    public static Grammar[] getGrammar()
+    {
+        Grammar grammar = { wolfram, open, google, delete, kill, wiki, tweet, recommend };
+        return grammar;
+    }
+}
