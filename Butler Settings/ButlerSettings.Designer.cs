a7b6a67985781fed899f041a63b7defadd70865a
Chukwudi Derek Uche <cuche@purdue.edu>
2015-02-22 23:59:24 -0500

    Time to make this all C#

--- Butler Settings/ButlerSettings.Designer.cs
+++ Butler Settings/ButlerSettings.Designer.cs
@@ -30,15 +30,25 @@
         {
             this.components = new System.ComponentModel.Container();
             this.metroStyleExtender1 = new MetroFramework.Components.MetroStyleExtender(this.components);
+            this.mainTabControl = new MetroFramework.Controls.MetroTabControl();
             this.SuspendLayout();
+            // 
+            // mainTabControl
+            // 
+            this.mainTabControl.Location = new System.Drawing.Point(23, 63);
+            this.mainTabControl.Name = "metroTabControl1";
+            this.mainTabControl.Size = new System.Drawing.Size(1039, 50);
+            this.mainTabControl.TabIndex = 0;
+            this.mainTabControl.UseSelectable = true;
             // 
             // Butler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1085, 495);
-            this.Name = "Butler";
-            this.Text = "Butler";
+            this.Controls.Add(this.metroTabControl1);
+            this.Name = "Butler Settings";
+            this.Text = "Butler Settings";
             this.Load += new System.EventHandler(this.Butler_Load);
             this.ResumeLayout(false);
 
@@ -47,6 +57,7 @@
         #endregion
 
         private MetroFramework.Components.MetroStyleExtender metroStyleExtender1;
+        private MetroFramework.Controls.MetroTabControl mainTabControl;
     }
 }
 
